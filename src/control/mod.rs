6589c2fbfd1d21b51e7b6814a9eff7c512e58136
Shishir Dey <shishirdey241097@gmail.com>
2022-02-17 14:00:06 +0530

    Add more components

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -1,1 +1,2 @@
 pub mod pid;
+pub mod bode_plot;
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 pub mod battery;
 pub mod charge_controller;
 pub mod control;
+pub mod data;
 pub mod inverter;
 pub mod math;
 pub mod phase_locked_loop;
