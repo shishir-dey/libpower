0026a9124788b2a688875e45a766ee50935fd46e
shishir-dey <shishir-dey@users.noreply.github.com>
2025-08-01 07:49:56 +0000

    deploy: 916074d81b29cadafdd42de2ad341c7ded0f36cc

---
+++ trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libpower",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"libpower/battery/soc/struct.BatteryInfo.html\" title=\"struct libpower::battery::soc::BatteryInfo\">BatteryInfo</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"libpower/battery/soc/struct.BatteryParameters.html\" title=\"struct libpower::battery::soc::BatteryParameters\">BatteryParameters</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"libpower/battery/soc/struct.PolynomialCoefficients.html\" title=\"struct libpower::battery::soc::PolynomialCoefficients\">PolynomialCoefficients</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[956]}
