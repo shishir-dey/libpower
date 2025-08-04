69af5e08440e0d633f9ab62bfa1ff68da8b58a54
shishir-dey <shishir-dey@users.noreply.github.com>
2025-08-04 02:34:38 +0000

    deploy: 4db463713cc8750e297e084caf3df8171a43364d

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libpower",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"libpower/signal/signal/struct.SignalMetrics.html\" title=\"struct libpower::signal::signal::SignalMetrics\">SignalMetrics</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"libpower/transform/clarke/struct.ClarkeResult.html\" title=\"struct libpower::transform::clarke::ClarkeResult\">ClarkeResult</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[617]}
